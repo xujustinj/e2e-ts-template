4d70c252225dbc06233fb80307d8652569fd943e
Justin Xu <xu.justin.j@gmail.com>
2022-01-14 01:14:46 -0500

    client: Set up SSR with tRPC

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,27 @@
 import { withTRPC } from "@trpc/next";
+// import config from "@xujustinj/common-config"; // TODO
 import type { AppProps } from "next/app";
+
 import "../styles/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
+const SERVER_URL = process.env["SERVER_URL"]!;
 export default withTRPC({
+  ssr: true, // https://trpc.io/docs/ssr
   config() {
-    return {
-      url: process.env.SERVER_URL!,
-    };
+    return process.browser
+      ? {
+          // client request, no SSR
+          url: SERVER_URL,
+        }
+      : {
+          url: SERVER_URL,
+          headers: {
+            "x-ssr": "1", // inform server that this is an ssr request
+          },
+        };
   },
 })(App);
