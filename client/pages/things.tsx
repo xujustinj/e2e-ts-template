220d294fa5e8d4f6f4bc64954b0103ddf69f5971
Justin Xu <xu.justin.j@gmail.com>
2022-01-31 15:00:00 -0500

    client: Make Things page interactive
    Make the styles prettier while at it

--- client/pages/things.tsx
+++ client/pages/things.tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useCallback, useState } from "react";
 import { trpc } from "../utils/trpc";
 
+// TODO: styled-components
+import styles from "../styles/Things.module.css";
+
 const Things: NextPage = () => {
-  // const utils = trpc.useContext();
+  const utils = trpc.useContext();
   const thingsQuery = trpc.useQuery(["things.read"]);
-  // const createThing = trpc.useMutation("things.create", {
-  //   async onSuccess() {
-  //     await utils.invalidateQueries("things.read");
-  //   },
-  // });
+  const thingMutation = trpc.useMutation("things.create", {
+    async onSuccess() {
+      await utils.invalidateQueries("things.read");
+    },
+  });
+
+  const [description, setDescription] = useState("");
+  const createThing = useCallback(() => {
+    thingMutation.mutate({ description }), setDescription("");
+  }, [thingMutation, description]);
 
   return (
     <div className={styles.container}>
@@ -23,9 +31,23 @@ const Things: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>{thingsQuery.data?.things.length ?? 0} Things</h1>
-      {thingsQuery.data?.things.map(({ id, description }) => (
-        <div key={id.toString()}>{description}</div>
-      ))}
+      <div className={styles["thing-container"]}>
+        {thingsQuery.data?.things.map(({ id, description }) => (
+          <div key={id.toString()} className={styles["thing-card"]}>
+            {description}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={createThing}>
+        <label>Create New Thing</label>
+        <br />
+        <input
+          placeholder="Description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <input type="submit" value="Create"></input>
+      </form>
     </div>
   );
 };
